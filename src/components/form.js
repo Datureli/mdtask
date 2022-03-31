325fc91851d6788d52e8059eb90c9721847f0b0b
Datureli <soleris1@gmail.com>
2022-03-31 23:18:07 +0200

    get rid off react warning - reactdomrender is no longer supported

--- src/components/form.js
+++ src/components/form.js
@@ -2,8 +2,13 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 const Form = () => {
-  const { register,formState: { errors }, handleSubmit } = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
   const onSubmit = (data) => console.log(data);
+
   return (
     <form className="formCard" onSubmit={handleSubmit(onSubmit)}>
       <label>
@@ -16,23 +21,31 @@ const Form = () => {
             maxLength: 20,
           })}
         />
+        {errors.productName?.type === "required" && "product name is required"}
       </label>
       <label>
         Description:
         <input type="text" {...register("description", { required: true })} />
-        {errors.description.type === 'required' && "First name is required"}
+        {errors.description?.type === "required" && "description is required"}
       </label>
-      <select {...register("type")}>
-        <option value="">podzespoły</option>
-        <option value="">urządzenia peryferyjne</option>
-        <option value="">Oprogramowanie</option>
-        <option value="">Inne</option>
-      </select>
-      <input
-        type="number"
-        {...register("price", { required: true, min: 1, max: 99 })}
-      />
-      <input type="submit" value="Wyślij" />
+      <label>
+        <select {...register("type")}>
+          <option value="">podzespoły</option>
+          <option value="">urządzenia peryferyjne</option>
+          <option value="">Oprogramowanie</option>
+          <option value="">Inne</option>
+        </select>
+        {errors.type?.type === "required" && "type is required"}
+      </label>
+      <label>
+        Price:
+        <input
+          type="number"
+          {...register("price", { required: true, min: 1, max: 99 })}
+        />
+        <input type="submit" value="Wyślij" />
+        {errors.price?.type === "required" && "price is required"}
+      </label>
     </form>
   );
 };
