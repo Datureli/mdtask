f7c25a3df7b7923f53d3e374bb32271057ba406b
Datureli <soleris1@gmail.com>
2022-03-31 19:06:33 +0200

    create navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Navbar = () => {
+  return (
+    <nav className="App-header">
+      <div>
+        <h1>Best software</h1>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
