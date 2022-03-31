588dc7624b15fdab4f32a174db82300b52e30440
Datureli <soleris1@gmail.com>
2022-03-31 21:12:02 +0200

    create validation on inputs

--- src/components/form.js
+++ src/components/form.js
@@ -1,15 +1,40 @@
-import {useForm} from 'react-hook-form'
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const Form = () => {
-    return (
-        <form onSubmit={this.handleSubmit}>
-        <label>
-          Imię:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Wyślij" />
-      </form>
-    )
-}
+  const { register,formState: { errors }, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  return (
+    <form className="formCard" onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        ProductName
+        <input
+          type="text"
+          {...register("productName", {
+            required: true,
+            minLength: 3,
+            maxLength: 20,
+          })}
+        />
+      </label>
+      <label>
+        Description:
+        <input type="text" {...register("description", { required: true })} />
+        {errors.description.type === 'required' && "First name is required"}
+      </label>
+      <select {...register("type")}>
+        <option value="">podzespoły</option>
+        <option value="">urządzenia peryferyjne</option>
+        <option value="">Oprogramowanie</option>
+        <option value="">Inne</option>
+      </select>
+      <input
+        type="number"
+        {...register("price", { required: true, min: 1, max: 99 })}
+      />
+      <input type="submit" value="Wyślij" />
+    </form>
+  );
+};
 
-export default Form
+export default Form;
