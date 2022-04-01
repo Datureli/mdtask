ea5728317b52c672242683e7d303a4d012eab2e6
Datureli <soleris1@gmail.com>
2022-04-01 17:44:15 +0200

    add map function over completed forms

--- src/components/form.js
+++ src/components/form.js
@@ -1,52 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const Form = () => {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const [completedForms, setCompletedForms] = useState([]);
+  const {register, formState: { errors }, handleSubmit,} = useForm();
+  const onSubmit = (data) => {
+    setCompletedForms(oldArray => [...oldArray,data])
+    console.log(data);
+  } 
 
   return (
-    <form className="formCard" onSubmit={handleSubmit(onSubmit)}>
-      <label>
-        ProductName
-        <input
-          type="text"
-          {...register("productName", {
-            required: true,
-            minLength: 3,
-            maxLength: 20,
-          })}
-        />
-        {errors.productName?.type === "required" && "product name is required"}
-      </label>
-      <label>
-        Description:
-        <input type="text" {...register("description", { required: true })} />
-        {errors.description?.type === "required" && "description is required"}
-      </label>
-      <label>
-        <select {...register("type")}>
-          <option value="">podzespoły</option>
-          <option value="">urządzenia peryferyjne</option>
-          <option value="">Oprogramowanie</option>
-          <option value="">Inne</option>
-        </select>
-        {errors.type?.type === "required" && "type is required"}
-      </label>
-      <label>
-        Price:
-        <input
-          type="number"
-          {...register("price", { required: true, min: 1, max: 99 })}
-        />
+    <div>
+      <form className="formCard" onSubmit={handleSubmit( onSubmit )}>
+        <label>
+          ProductName
+          <input
+            type="text"
+            {...register("productName", {
+              required: true,
+              minLength: 3,
+              maxLength: 20,
+            })}
+          />
+          {errors.productName?.type === "required" &&
+            "product name is required"}
+        </label>
+        <label>
+          Description:
+          <input type="text" {...register("description", { required: true })} />
+          {errors.description?.type === "required" && "description is required"}
+        </label>
+        <label>
+          Category:
+          <select {...register("type")}>
+            <option value="podzespoły">podzespoły</option>
+            <option value="urządzenia peryferyjne">
+              urządzenia peryferyjne
+            </option>
+            <option value="Oprogramowanie">Oprogramowanie</option>
+            <option value="Inne">Inne</option>
+          </select>
+          {errors.type?.type === "required" && "type is required"}
+        </label>
+        <label>
+          Price:
+          <input
+            type="number"
+            {...register("price", { required: true, min: 1, max: 20000 })}
+          />
+          {errors.price?.type === "required" && "price is required"}
+        </label>
         <input type="submit" value="Wyślij" />
-        {errors.price?.type === "required" && "price is required"}
-      </label>
-    </form>
+      </form>
+      
+      <ul>
+        {completedForms &&
+          completedForms.map((form, index) => <li key={index}>
+           {form.productName}
+           {form.description}
+           {form.type}
+           {form.price}
+          </li>)}
+      </ul>
+    </div>
   );
 };
 
