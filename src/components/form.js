ed2b5669d109a75b98dc5dcdbaef8b3394f8c5e8
Datureli <soleris1@gmail.com>
2022-04-02 15:58:34 +0200

    add product counter

--- src/components/form.js
+++ src/components/form.js
@@ -2,24 +2,27 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 const Form = () => {
-    const { register, formState: { errors }, handleSubmit} = useForm({
-        defaultValues: {
-            products: [{productName: 'komputer',description: 'opis',type: '',price: 0}]
-        }
+  let [productCounter,setProductCounter] = useState(0)
+  const { register,formState: { errors },handleSubmit,} = useForm({
+    defaultValues: {
+      products: [
+        { productName: "komputer", description: "opis", type: "", price: 0 },
+      ],
+    },
   });
   const [completedForms, setCompletedForms] = useState([]);
 
   const onSubmit = (data) => {
     setCompletedForms((oldArray) => [...oldArray, data]);
+    setProductCounter(productCounter + 1)
     console.log(data);
   };
   const removeProduct = (index) => {
-    setCompletedForms([...completedForms.slice(0, index), ...completedForms.slice(index + 1)]);
+    setCompletedForms([
+      ...completedForms.slice(0, index),
+      ...completedForms.slice(index + 1),
+    ]);
   };
-  const totalPrice = () => {
-
-  };
-
   return (
     <div>
       <form className="formCard" onSubmit={handleSubmit(onSubmit)}>
@@ -91,7 +94,7 @@ const Form = () => {
         <input type="submit" value="Wyślij" />
       </form>
       <ul>
-        {completedForms &&
+        {completedForms &&  
           completedForms.map((form, index) => (
             <li key={index}>
               <p> Product name:{form.productName}</p>
@@ -99,11 +102,16 @@ const Form = () => {
               <p> type: {form.type}</p>
               <p> price: {form.price}</p>
               <button onClick={() => removeProduct(index)}>remove</button>
-             
             </li>
-          
           ))}
-           <h1>total price: {completedForms.reduce((total, form) => total+(parseInt(form.price)),0)}</h1>
+        <h2>
+          total price:{" "}
+          {completedForms.reduce(
+            (total, form) => total + parseInt(form.price),
+            0
+          )}
+        </h2>
+        <h2 >Liczba produktów: {productCounter}</h2>
       </ul>
     </div>
   );
